695b88b5890af59e58bf64a8437e7f6c901cab4f
Bianca Luisa <ghostekkjk@gmail.com>
2025-04-25 11:44:34 -0300

    Array 10

--- array10.js
+++ array10.js
@@ -1,8 +1,8 @@
 let jogos = ['Sally Face', 'Valorant', 'Stardew Valley'];
 let jogoFav = 'Stardew Valley';
 
-for(let i = 0; i < jogos.length; i++){
-    if(jogoFav == jogos[i]){
+for (let i = 0; i < jogos.length; i++) {
+    if (jogoFav == jogos[i]) {
         console.log(`o meu jogo favorito é ${jogos[i]} e está na posição ${i + 1}`);
     }
 }

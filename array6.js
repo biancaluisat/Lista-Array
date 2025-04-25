8531a622625a73e2c4de0cd038ee6bc5734c901a
Bianca Luisa <ghostekkjk@gmail.com>
2025-04-25 11:46:08 -0300

    Array 6

--- array6.js
+++ array6.js
@@ -1,5 +1,5 @@
 let itens = ['Pente', 'Creme', 'Shampoo', 'Condicionador'];
 
-for(let i = itens.length -1; i > -1; i--){
+for (let i = itens.length - 1; i > -1; i--) {
     console.log(itens[i]);
 }

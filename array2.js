0c14f774256dfbb16a498285fdaa9d872a87622b
Bianca Luisa <ghostekkjk@gmail.com>
2025-04-25 09:47:03 -0300

    Array 2

---
+++ array2.js
@@ -0,0 +1,8 @@
+let playlist = ['Sunff', 'Homesick', 'Love You Should Come Over', 'No One Notice'];
+let quantidade = playlist.length;
+
+
+
+for(let i = 0; i < quantidade; i++){
+    console.log(playlist[i]);
+}

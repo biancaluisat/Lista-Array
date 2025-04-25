112a66fbef55277bd15049b284829ba1d491c9e6
Bianca Luisa <ghostekkjk@gmail.com>
2025-04-25 10:38:41 -0300

    Array 8

---
+++ array8.js
@@ -0,0 +1,8 @@
+let idades = [12, 14, 19, 20, 8]
+let quantidade = idades.length
+
+for(i = 0; i < quantidade; i++){
+    if(idades[i] >= 18){
+        console.log(`idade +18: ${idades[i]}`);
+    }
+}
